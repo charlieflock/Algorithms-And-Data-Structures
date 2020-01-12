c924b9e4669b279f9fd924160bbc2d0dc2f2d6ec
charlieflock <46385649+charlieflock@users.noreply.github.com>
2020-01-12 16:20:51 -0500

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,7 @@ public class InsertionSort {
     public int[] sort(int[] arr){
         for(int i = 1; i<arr.length ; i++){
             int j = i;
-            while( (j > 0) && (arr[j-1] > arr[j]) ){
+            while( j > 0 && arr[j-1] > arr[j] ){
                 int temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
