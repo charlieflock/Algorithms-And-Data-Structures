696bda754c3a24a23c1704ec8ef1f0d79ca63fd3
charlieflock <46385649+charlieflock@users.noreply.github.com>
2020-01-12 17:42:26 -0500

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    
+    public boolean search(int[]arr, int target){
+        int left = 0;
+        int right = arr.length -1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(arr[mid] == target)
+                return true;
+            else if(target < arr[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;   
+        } 
+      return false;  
+    }
+    
+}
